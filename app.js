860d0efb2520493820e34aab53464234bf4efa93
Caylin Vu <caylinvu@gmail.com>
2023-10-26 15:18:39 -0500

    Add form errors for logging in

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 require('dotenv').config();
 
 const User = require('./models/user');
@@ -35,11 +36,11 @@ passport.use(
     try {
       const user = await User.findOne({ email: username });
       if (!user) {
-        return done(null, false, { message: 'Incorrect email' });
+        return done(null, false, { message: 'Email does not exist, please try again' });
       }
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        return done(null, false, { message: 'Incorrect password' });
+        return done(null, false, { message: 'Incorrect password, please try again' });
       }
       return done(null, user);
     } catch (err) {
@@ -76,6 +77,9 @@ app.get('/log-in', (req, res, next) => {
   if (!req.user) {
     res.render('log-in-form', {
       title: 'Log in',
+      failureMessage: req.session.messages
+        ? req.session.messages[req.session.messages.length - 1]
+        : '',
     });
   } else {
     res.redirect('/');
@@ -85,9 +89,29 @@ app.get('/log-in', (req, res, next) => {
 // Handle log in on POST
 app.post(
   '/log-in',
+  body('username')
+    .isLength({ min: 1 })
+    .withMessage('Email is required')
+    .bail()
+    .isEmail()
+    .withMessage('Please enter valid email address'),
+  body('password', 'Password is required').isLength({ min: 1 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('log-in-form', {
+        title: 'Log in',
+        username: req.body.username,
+        password: req.body.password,
+        errors: errors.array(),
+      });
+    }
+    next();
+  },
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/log-in',
+    failureMessage: true,
   }),
 );
 
@@ -120,8 +144,6 @@ app.use(function (err, req, res, next) {
 module.exports = app;
 
 // REMAINING TO DO
-
-// add form errors for logging in
 
 // sanitize login data???
 
